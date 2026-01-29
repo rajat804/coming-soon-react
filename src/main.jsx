6a56577cedc64d00c062c8a6029135e3b21b142d
Rajat <rajat804@gmail.com>
2026-01-29 15:40:07 +0530

    Fix CSS, set ComingSoon as main page, remove App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ComingSoon from "./pages/CommingSoon"; // yahi component render hoga
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ComingSoon from './pages/CommingSoon';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ComingSoon />
   </React.StrictMode>
